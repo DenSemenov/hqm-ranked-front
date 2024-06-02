f8037157f26bffcd2bb7c6042488c84c02a5415b
Denis Semenov <denis.semenov@pix.ru>
2024-06-02 13:05:25 +0300

    Added left days

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 
-import { Avatar, Badge, Button } from 'antd';
+import { Avatar, Badge, Button, Tag } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import styles from './Header.module.css'
 import { useEffect, useMemo, useState } from 'react';
@@ -8,7 +8,7 @@ import { selectCurrentUser, selectIsAuth } from 'stores/auth';
 import { getCurrentUser } from 'stores/auth/async-actions';
 import { useAppDispatch } from 'hooks/useAppDispatch';
 import ChangePasswordModal from './ChangePasswordModal';
-import { selectStorageUrl } from 'stores/season';
+import { selectCurrentSeason, selectSeasons, selectStorageUrl } from 'stores/season';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ThemeButton from './ThemeButton';
 import { isMobile } from 'react-device-detect';
@@ -21,6 +21,8 @@ const Header = () => {
     const isAuth = useSelector(selectIsAuth);
     const currentUser = useSelector(selectCurrentUser);
     const storageUrl = useSelector(selectStorageUrl);
+    const currentSeason = useSelector(selectCurrentSeason);
+    const seasons = useSelector(selectSeasons);
 
     const [changePasswordModalOpen, setChangePasswordModalOpen] = useState<boolean>(false);
 
@@ -77,11 +79,25 @@ const Header = () => {
         }
     }, [isMobile, currentUser, userName, avatarName, isAuth])
 
+    const endsIn = useMemo(() => {
+        if (currentSeason) {
+            const current = seasons.find(x => x.id === currentSeason)
+            if (current) {
+                const leftDays = Math.round((new Date(current.dateEnd).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
+                return <Tag color="#108ee9" style={{ height: 20 }}>{leftDays + " days left"}</Tag>
+            }
+        }
+    }, [currentSeason, seasons])
+
+
     return (
         <>
-            <svg height="36" width="36" onClick={() => navigate("/")}>
-                <image href="/icons/logo.svg" height="36" width="36" />
-            </svg>
+            <div style={{ display: "flex", gap: 16, alignItems: "center" }}>
+                <svg height="36" width="36" onClick={() => navigate("/")}>
+                    <image href="/icons/logo.svg" height="36" width="36" />
+                </svg>
+                {endsIn}
+            </div>
             <div className={styles.headerContainerLogin}>
                 <ThemeButton />
                 {loginButton}
